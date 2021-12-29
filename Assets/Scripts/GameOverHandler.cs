eb308c96b3bef67baed28292bb5353067a4d6d2a
Ilya <ilya.balashow1@gmail.com>
2021-12-29 20:37:01 +0600

    Added score system

--- Assets/Scripts/GameOverHandler.cs
+++ Assets/Scripts/GameOverHandler.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameOverHandler : MonoBehaviour
 {
+
+    [SerializeField] private TMP_Text gameOverText;
+    [SerializeField] private ScoreSystem scoreSystem;
     [SerializeField] private GameObject gameOverDisplay;
     [SerializeField] private AsteroidSpawner asteroidSpawner;
     public void EndGame()
     {
         asteroidSpawner.enabled = false;
 
+        gameOverText.text = $"Your score: {scoreSystem.EndTimer().ToString()}";
+      
         gameOverDisplay.gameObject.SetActive(true);
     }
     public void PlayAgain()
