617034fd2d37a81535b0065156c131c87e3f7dfb
Ilya <ilya.balashow1@gmail.com>
2022-03-13 13:58:52 +0600

    Removed all unnecessary using directives

--- Assets/Scripts/GameOverHandler.cs
+++ Assets/Scripts/GameOverHandler.cs
@@ -1,16 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameOverHandler : MonoBehaviour
 {
-
     [SerializeField] private TMP_Text gameOverText;
     [SerializeField] private ScoreSystem scoreSystem;
     [SerializeField] private GameObject gameOverDisplay;
     [SerializeField] private AsteroidSpawner asteroidSpawner;
+
     public void EndGame()
     {
         asteroidSpawner.enabled = false;
